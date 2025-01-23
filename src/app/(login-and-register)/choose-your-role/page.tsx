e5407e1a552605803e26595f1ad1e168923ffaf8
Robinson Lam <robinsonlam94@gmail.com>
2025-01-23 13:34:39 +1100

    remove unused files

--- src/app/(login-and-register)/choose-your-role/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChooseYourRole() {
-    return (
-        <div>
-            <h1>ChooseYourRole</h1>
-        </div>
-    )
-}
--- src/app/(login-and-register)/select-services/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SelectServices() {
-    return (
-        <div>
-            <h1>SelectServices</h1>
-        </div>
-    )
-}
