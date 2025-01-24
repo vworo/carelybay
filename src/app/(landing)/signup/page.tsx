82d233f47432f549fc759f43e7877ab0b6688e79
vworo <victorworobec.gmail.com>
2025-01-25 01:16:38 +1100

    add base MaterialUI components to signup route

--- src/app/(landing)/signup/page.tsx
+++ src/app/(landing)/signup/page.tsx
@@ -1,5 +1,5 @@
 import PageHeader from "@/app/components/layout/page-header";
-import { Container } from "@mui/material";
+import { Container, Input, FormGroup, FormControlLabel, Checkbox, Typography, Link, Button, Divider } from "@mui/material";
 import { UsersStep } from "./(2_2_users)/users";
 
 export default function SignUp() {
@@ -9,6 +9,24 @@ export default function SignUp() {
         heading="Sign up"
         subtitle="Enter your details to sign-up for a new Carelybay account!"
       />
+      <Input placeholder="Email*" type="email" fullWidth />
+      <Input placeholder="Create a password" type="password" fullWidth />
+      <FormControlLabel 
+        control={<Checkbox />} 
+        label={
+          <Typography>* I agree to the {" "}
+            <Link href="/terms-of-service" target="_blank" rel="noopener">Terms of Service</Link>
+            {" "} and {" "}
+            <Link href="/privacy-policy" target="_blank" rel="noopener"> Privacy Policy</Link>
+          </Typography>
+        } 
+      />
+      <Button fullWidth>Sign up</Button>
+      <Divider>Or</Divider>
+      <Button fullWidth>Sign up with Google</Button>
+      <Button fullWidth>Sign up with Facebook</Button>
+      <Typography style={{ textAlign: "center"}}>Already using Carelybay? <Link href="/signin">Sign in</Link></Typography>
+      {/* TODO: Remove after testing */}
       <UsersStep />
     </Container>
   );
